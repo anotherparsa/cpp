d9e7430fa871dd1104207b9f5bf7d4fedd927931
anotherparsa <anotherparsa@gmail.com>
2025-07-27 11:13:53 +0330

    defining classes

---
+++ data_structures/singly_linked_list/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+        }
+};
+
+class LinkedList{
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+    
+    public:
+        LinkedList(){
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->length = 0;
+        }
+};
