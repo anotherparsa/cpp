d9ab907daa1dc15ce2112e6e42fc2fbdbbf6fd86
anotherparsa <anotherparsa@gmail.com>
2025-08-04 18:27:12 +0330

    doubly linked list

---
+++ data_structures/linked_lists/doubly_linked_list/dll.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+        Node* before;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+            this->before = nullptr;
+        }
+};
+
+class DLL{
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    public:
+        DLL(){
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->length = 0;
+        }
+};
+
+int main(){
+    
+}
