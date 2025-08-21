de4700d32354f1ffbefda0b48cbe80137f9be3ed
anotherparsa <anotherparsa@gmail.com>
2025-08-21 08:25:01 +0330

    append node method

--- Data_structure/Singly_linked_list/sll.cpp
+++ Data_structure/Singly_linked_list/sll.cpp
@@ -27,11 +27,38 @@ class SLL{
         }
 
         void print_list(){
-            Node* temp_node = this->head;
-            cout << "List Items: " << endl;
-            while (temp_node != nullptr){
-                cout << temp_node->value << " ";
+            if (this->length == 0){
+                cout << "List is empty: " << endl;
+            }else{
+                Node* temp_node = this->head;
+                cout << "List Items: " << endl;
+                while (temp_node != nullptr){
+                    cout << temp_node->value << " ";
+                    temp_node = temp_node->next;
+                }
+                cout << endl;
             }
-            cout << endl;
+        }
+
+        void append_node(int value){
+            Node* new_node = new Node(value);
+            if (this->length == 0){
+                this->head = new_node;
+                this->tail = new_node;
+            }else{
+                this->tail->next = new_node;
+                this->tail = new_node;
+            }
+            this->length++;
         }
 };
+
+int main(){
+    SLL* mysll = new SLL();
+    mysll->print_list();
+    mysll->append_node(1);
+    mysll->append_node(2);
+    mysll->append_node(3);
+    mysll->append_node(4);
+    mysll->print_list();
+}
