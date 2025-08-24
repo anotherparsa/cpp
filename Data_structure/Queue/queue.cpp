7d7dceff44ee5cf373750bb435e453d0ec74b50b
anotherparsa <anotherparsa@gmail.com>
2025-08-24 07:20:32 +0330

    print queue method in queue

--- Data_structure/Queue/queue.cpp
+++ Data_structure/Queue/queue.cpp
@@ -25,4 +25,19 @@ class Queue{
             this->last = nullptr;
             this->length = 0;
         }
-}
+
+        void print_queue(){
+            if (this->length == 0){
+                cout << "Queue is empty" << endl;
+                return;
+            }else{
+                Node* temp_node = this->first;
+                cout << "Queue elements: " << endl;
+                while (temp_node != nullptr){
+                    cout << temp_node->value << " -> "; 
+                    temp_node = temp_node->next;
+                }
+                cout << endl << "Null"
+            }
+        }
+};
