fd4132d40724e15aeb884d16d4da239ba092933f
anotherparsa <anotherparsa@gmail.com>
2025-08-24 04:09:05 +0330

    prepend node method in dll

--- Data_structure/Linked_list/Doubly_linked_list/dll.cpp
+++ Data_structure/Linked_list/Doubly_linked_list/dll.cpp
@@ -72,6 +72,19 @@ class DLL{
             this->length--;
         }
 
+        void prepend_node(int value){
+            Node* new_node = new Node(value);
+            if (this->length == 0){
+                this->head = new_node;
+                this->tail = new_node;
+            }else{
+                new_node->next = this->head;
+                this->head->previous = new_node;
+                this->head = new_node;
+            }
+            this->length++;
+        }
+
 };
 
 int main(){
@@ -88,5 +101,10 @@ int main(){
     mydll->print_list();
     mydll->delete_last_node();
     mydll->delete_last_node();
+    mydll->print_list();
+    mydll->prepend_node(1);
+    mydll->prepend_node(2);
+    mydll->prepend_node(3);
+    mydll->prepend_node(4);
     mydll->print_list();
 }
